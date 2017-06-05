9aa1dcd93d78ecfbb60cf430076c05183b0799ab
Anoop Singh <anoop100singh@gmail.com>
2017-06-05 23:23:36 +0530

    Added socket code to angular controller

--- Client/js/app.js
+++ Client/js/app.js
@@ -1,17 +1,88 @@
-var app = angular.module('myApp',[])
+var app = angular.module('myApp',['ngMaterial','ui.router','ngStorage'])
 
-app.controller('mainController',['$scope',function($scope){
-   var socket = io.connect()
-   $scope.send = function() {
-	 console.log($scope.message)  
-	socket.emit('chat message',$scope.message)
-    $scope.message="";	
-   }
-      
-    socket.on('chat message', function(msg){
-		var li=document.createElement("li");
-        li.appendChild(document.createTextNode(msg));
-        document.getElementById("messages").appendChild(li);
-	})  
+app.factory('socket',function(){
+	var socket = io.connect()
+	return(
+	   on: function(eventName, callback){
+		   socket.on(eventName, callback)
+	   },
+	   
+	   emit: function(eventName,data){
+		   socket.emit(eventName,data)
+	   }   
+	)
 	
+})
+
+app.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
+	$urlRouterProvider.otherwise('/')
+	
+	$stateProvider
+	.state('login',{
+		url: '/',
+		views:{
+			'body': {
+				templateUrl: '/Client/login.html', 
+				controller: 'registerController' 
+			}
+			
+		}
+		
+		
+	})
+	
+	.state('loggedin',{
+	   url:	'/login',
+	   views: {
+		   'body': {
+			   templateUrl: '/Client/chat.html',
+			   controller: 'myController'
+		   }
+	   }
+	})
 }])
+
+app.directive('myEnter',function(){
+	return function(scope,element,attres)
+	{
+		element.bind('keydown keypress',function(event)
+		{
+			if(event.which === 13)
+			{
+				scope.$apply(function(){
+					scope.$eval(attres.myEnter)
+				})
+				event.preventDefault()
+			}
+		})
+	}
+})
+
+app.controller('mycontroller',['$scope','socket','$http','$mdDialog','$compile','$location','$state','$localStorage','$sessionStorage',function($scope,socket,$http,$mdDialog,$compile,$location,$state,$localStorage,$sessionStorage)
+{
+	url = location.host
+	$scope.user=[]
+	$scope.online_friends=[]
+	$scope.allfriends=[]
+	$scope.messages={}
+	var monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October","November", "December"];
+	
+	socket.on('handle',function(data){
+		$scope.user = data
+	})
+	
+	socket.on('friend_list',function(data){
+		$scope.$apply(function(){
+		   $scope.allfriends.push.apply($scope.allfriends,data)	
+		})
+	})
+	
+	socket.on('users',function(data){
+		$scope.$apply(function()
+		{
+			
+		})
+	})
+}
+])
+
